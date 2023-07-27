cadbe0ce5e2fa5b1080c7b27e80dc0c058a82449
Adewale Fatoki <adewaleftk@gmail.com>
2023-07-27 23:54:16 +0100

    Javascript Classes

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,13 @@
+import ClassRoom from './0-classroom';
+
+function initializeRooms() {
+  const classrooms = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
+
+  return classrooms;
+}
+
+export default initializeRooms;
