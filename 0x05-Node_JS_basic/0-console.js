1039d6460b64b90b70dd985f25906b0d42e28ef3
Adewale Fatoki <adewaleftk@gmail.com>
2023-09-19 01:52:22 +0100

    updated

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Adewale Fatoki <https://github.com/adewaleftk>
+ */
+function displayMessage(message) {
+    console.log(message); // This will print the message to STDOUT
+  }
+
+module.exports = displayMessage;
